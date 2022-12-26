008d8d06fe689c5c042d522ba4c0a077f91733d9
SardarBadar <dataentrybadar@gmail.com>
2022-12-26 18:55:38 +0500

    Code cleaning

--- app/src/main/java/org/blind/help/object/detection/MainActivity.java
+++ app/src/main/java/org/blind/help/object/detection/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
 
         // prevent Main Activity
         Intent intent =new Intent(MainActivity.this, DetectorActivity.class);
